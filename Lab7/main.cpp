75fb7306775018158b84b9c6b2dca652229e24a0
Cameron Brown <cameronjoebrown@gmail.com>
2018-11-25 15:40:23 -0700

    Initial Commit

---
+++ Lab7/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab7
+//
+//  Created by Cam on 11/25/18.
+//  Copyright © 2018 Cameron Brown. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
